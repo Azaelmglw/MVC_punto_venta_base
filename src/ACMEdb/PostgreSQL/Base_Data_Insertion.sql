ef1c3fc8b656f9c6a1da0b2058ed9f7489e28e5d
Azaelmglw <>
2018-11-05 12:00:07 -0600

    Documentation

---
+++ src/ACMEdb/PostgreSQL/Base_Data_Insertion.sql
@@ -0,0 +1,16 @@
+/**
+ * Author:  azaelmglw
+ * Created: Nov 5, 2018
+ */
+
+
+INSERT INTO sucursales(SucursalID, Numero, Colonia, Codigo_Postal, Email, Telefono, Ciudad, Estado)
+VALUES
+('SU0', '42', 'Pedregales', '48957', 'acmesu0@gmail.com', '775-128-63-89', 'Tulancingo', 'Hidalgo');
+
+
+INSERT INTO proveedores(ProveedorID, Nombre, Apellido_Paterno, Apellido_Materno, Empresa, Telefono, Calle, 
+Colonia, Numero, Codigo_Postal, Email, Ciudad, Estado)
+VALUES
+('PR0', 'Carlos', 'Ballarta', 'Hernandez', 'Truper', '775-105-54-55', 'Pajaritos', 'Lindavista', '407', '43680', 'caba@gmail.com', 'Tulancingo', 'Hidalgo');
+
